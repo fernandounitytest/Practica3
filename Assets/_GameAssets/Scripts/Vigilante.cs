97de19efdb5b7c9ad555d9d158ae31d45477e979
Fernando Paniagua <fernando.paniagua@gmail.com>
2018-11-06 22:44:41 +0100

    Initial commit

---
+++ Assets/_GameAssets/Scripts/Vigilante.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Vigilante : MonoBehaviour {
+    public Transform target;
+    NavMeshAgent agente;
+	
+	void Start () {
+        agente = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        agente.destination = target.position;
+    }
+}
